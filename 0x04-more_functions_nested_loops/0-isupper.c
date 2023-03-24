bb0b5974e88b1a5254a819a45e1893244a2650da
keffakevin <kabagambe400@gmail.com>
2023-03-24 15:29:09 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - function that print alahabetic
+ * @c:parameter description
+ * Return:1 and return 0 otherwise
+ */
+
+int _isupper(int c)
+{
+if (x <= 65 && x <= 98)
+{
+return (1);
+}
+retun (0);
+}
